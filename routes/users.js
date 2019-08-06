f25eb4641f404dd056617d2ee6f931e0eb2ea3f6
DivyanshBatham <divyansh.sunita@gmail.com>
2019-08-07 01:18:57 +0530

    Changed  to

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/user.js')
 

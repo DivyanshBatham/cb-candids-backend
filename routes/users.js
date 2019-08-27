9b9de4e904369934e36bacd6f8d19e72da12d5d8
DivyanshBatham <divyansh.sunita@gmail.com>
2019-08-27 16:07:57 +0530

    Added User Basic Routes
    - `{{domain}}/users` - For debugging only.
    - `{{domain}}/users/{{userName}}` - returns user details, postsCount, likesCount and authoredPosts.

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,73 @@
 const express = require('express');
-const router = express.Router();
+const userRouter = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/user.js')
+const Post = require('../models/post.js')
+const jwtAuthCheck = require('../helpers/jwtAuthCheck');
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  User.find({}).then(res => console.log(res));
-
-  const user = new User({
-    _id: new mongoose.Types.ObjectId(),
-    username: "Name",
-    email: "Email"
+// Fetch all Users (DEBUGGING ONLY)
+userRouter.get("/", jwtAuthCheck, (req, res) => {
+  User.find().then(users => {
+    console.log("Users => ", users);
+    res.status(200).json({
+      "success": true,
+      "data": {
+        users: users,
+      },
+    });
+  }).catch(err => {
+    console.log(err)
+    res.status(500).json({
+      "success": false,
+      "errors": err.message
+    });
   })
-  
-  // Turns into promise:
-  user.save().then(result => {
-    console.log(result);
-  })
-  .catch(err => console.err(err))
-  
-  res.status(201).json({
-    message: "User create Successfully",
-    createdUser: user
-  });
-
 });
 
-module.exports = router;
+// Fetch specific User:
+userRouter.get("/:userName", jwtAuthCheck, (req, res) => {
+  const { userName } = req.params;
+
+  User.findOne({ username: userName }).then(user => {
+    if (user) {
+      Post.find({ author: user.id })
+        .populate({ path: 'author', model: User, select: ['username', 'email'] })
+        .populate({ path: 'likes', model: User, select: 'username' })
+        .populate({ path: 'taggedUsers', model: User, select: 'username' })
+        .populate({ path: 'comments.author', model: User, select: 'username' })
+        .populate({ path: 'comments.likes', model: User, select: 'username' })
+        .then(posts => {
+          res.status(200).json({
+            "success": true,
+            "data": {
+              user: user,
+              postCount: posts.length,
+              likeCount: posts.reduce((count, post) => count + post.likes.length, 0),
+              posts: posts,
+            },
+          });
+        }).catch(err => {
+          console.log(err)
+          res.status(500).json({
+            "success": false,
+            "errors": err.message
+          });
+        })
+
+    }
+    else
+      res.status(404).json({
+        "success": false,
+        "errors": "User not Found"
+      });
+  }).catch(err => {
+    console.log(err)
+    res.status(500).json({
+      "success": false,
+      "errors": err.message
+    });
+  })
+});
+
+
+module.exports = userRouter;
